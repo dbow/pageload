f9a61250f5f59ce9a57db31b941aa32c144ad1de
bowman224 <dbowman123@gmail.com>
2012-02-10 03:31:41 -0800

    Initial commit - left out rest of HTML5boilerplate files

---
+++ js/script.js
@@ -0,0 +1,58 @@
+/* Author: dbow (d-bow.com)
+
+*/
+
+var Test = {};
+
+Test.Dom = (function () {
+  
+  var me = {};
+  
+  me.setup = function () {
+    
+    var divHtml = '<div class="hidden">TESTTESTTEST</div>',
+        imgDivHtmlStart = '<div class="hidden"><img src="',
+        imgDivHtmlEnd = '"/></div>',
+        i,
+        fillerHtml = '',
+        j,
+        imgList = ['http://www.gstatic.com/hostedimg/0ac14c8287d89935_landing',
+                   'http://www.gstatic.com/hostedimg/e2a28ef2b0dd9178_landing',
+                   'http://www.gstatic.com/hostedimg/124cf5d4c25bd8d0_landing',
+                   'http://www.gstatic.com/hostedimg/654f1cf21c26f452_landing',
+                   'http://www.gstatic.com/hostedimg/a1222d69800c8118_landing',
+                   'http://www.gstatic.com/hostedimg/be30eb00e5d18259_landing',
+                   'http://www.gstatic.com/hostedimg/6045385199dcd4de_landing',
+                   'http://www.gstatic.com/hostedimg/a190b4b88823437f_landing',
+                   'http://www.gstatic.com/hostedimg/38ed7740f45789a0_landing',
+                   'http://www.gstatic.com/hostedimg/25128785f41ec47e_landing',
+                   'http://www.gstatic.com/hostedimg/81360443f9dd01d2_landing',
+                   'http://www.gstatic.com/hostedimg/fa68cf5827caf3fe_large',
+                   'http://www.gstatic.com/hostedimg/b87eea17f879eea1_landing',
+                   'http://www.gstatic.com/hostedimg/21dca153d258ff3a_landing',
+                   'http://www.gstatic.com/hostedimg/1e22f520e491cc2b_landing'],
+        imgListLength = imgList.length;
+
+    for (i=0; i < 1000; i++) {
+      fillerHtml += divHtml;
+    }
+    for (j=0; j<imgListLength; j++) {
+      fillerHtml += (imgDivHtmlStart + imgList[j] + imgDivHtmlEnd);      
+    }
+    $('#filler').html(fillerHtml);
+    
+  };
+  
+  return me;
+  
+})();
+
+
+$(function () {
+
+  Test.Dom.setup();
+
+});
+
+
+
